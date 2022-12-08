a6e2dd45c843df4863470c664dee5a1bdae9c6d4
Husnain_Khurshid <muhammadhusnainkh@gmail.com>
2022-12-08 16:49:32 +0500

    Connection

--- main.go
+++ main.go
@@ -23,16 +23,22 @@ func CheckError(err error) {
 	}
 }
 
-func main() {
-
+func databseConnection() {
 	dbConnection := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	db, err := sql.Open("postgres", dbConnection)
 	CheckError(err)
 	defer db.Close() // Db connection will be closed after the main function completes its execution
 
-	// Inserting data into database //Hard Coded
-	insertData := `INSERT INTO "Vehicle".motor ("vehicle_name","vehicle_company") VALUES ('Civic','Honda')`
+	addData(db) // Inserting data into
+	fmt.Println("Connection Established Sucessfully!")
+}
+
+func addData(db *sql.DB) {
+	insertData := `INSERT INTO "vehicle".motor ("vehicle_name","vehicle_company") VALUES ('Pridor','Honda')`
 	_, e := db.Exec(insertData)
 	CheckError(e)
+}
 
+func main() {
+	databseConnection()
 }
