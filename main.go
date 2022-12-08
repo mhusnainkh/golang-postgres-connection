fe96aa635e9624c87d9f594a9dff0dcbcb0e875a
Husnain_Khurshid <muhammadhusnainkh@gmail.com>
2022-12-08 15:36:32 +0500

    Db Connection

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+// Database credentials
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "demodb"
+)
+
+// To check if there is any error
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	dbConnection := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", dbConnection)
+	CheckError(err)
+	defer db.Close() // Db connection will be closed after the main function completes its execution
+
+	insertData := `INSERT INTO "Vehicle".motor ("vehicle_name","vehicle_company") VALUES ('Civic','Honda')`
+	_, e := db.Exec(insertData)
+	CheckError(e)
+
+}
